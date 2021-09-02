09daed0b73defcab469a3369834822708fa34a01
mattd <102270274@student.swin.edu.au>
2021-09-02 19:57:01 +1000

    Commit and push LightMeUp

--- app/src/main/java/au/edu/swin/sdmd/myapplication/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/myapplication/MainActivity.kt
@@ -5,14 +5,19 @@ import android.os.Bundle
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-
+    var state = R.drawable.ic_thumb_up_black_48dp
     override fun onCreate(savedInstanceState: Bundle?) {
-        var state = R.drawable.ic_thumb_up_black_48dp
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val imageView = findViewById<ImageView>(R.id.imageView5)
+
+        savedInstanceState?.let{
+            state = it.getInt("IMAGE")
+            imageView.setImageDrawable(getDrawable(state))
+        }
+
         imageView.setOnLongClickListener {
             state = when (state) {
                 R.drawable.ic_thumb_up_black_48dp -> R.drawable.ic_thumb_up_off_alt_black_48dp
@@ -22,5 +27,10 @@ class MainActivity : AppCompatActivity() {
             imageView.setImageDrawable(getDrawable(state))
             true
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("IMAGE", state)
     }
 }
