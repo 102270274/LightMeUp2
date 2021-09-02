a82bea68dc9d71b083dae821d04d17f7fa81446f
mattd <102270274@student.swin.edu.au>
2021-09-02 19:46:21 +1000

    Commit and push LightMeUp

--- app/src/main/java/au/edu/swin/sdmd/myapplication/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/myapplication/MainActivity.kt
@@ -13,12 +13,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val imageView = findViewById<ImageView>(R.id.imageView5)
-        imageView.setOnClickListener {
+        imageView.setOnLongClickListener {
             state = when (state) {
                 R.drawable.ic_thumb_up_black_48dp -> R.drawable.ic_thumb_up_off_alt_black_48dp
+                R.drawable.ic_thumb_up_off_alt_black_48dp -> R.drawable.ic_thumb_up_black_48dp
                 else -> R.drawable.ic_thumb_up_black_48dp
             }
             imageView.setImageDrawable(getDrawable(state))
+            true
         }
     }
 }
